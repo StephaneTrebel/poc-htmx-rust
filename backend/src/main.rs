a34be118f5ab60f93be997153517885cf212552c
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2024-01-18 11:38:24 +0100

    Add Maud templating engine

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,15 @@
 use axum::{routing::get, Router};
+use maud::{html, Markup};
+
+async fn hello_world() -> Markup {
+    html! {
+        h1 { "Hello, World!" }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World !\n" }));
+    let app = Router::new().route("/", get(hello_world));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
