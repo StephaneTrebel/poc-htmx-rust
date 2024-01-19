1eef6da4966ae6dc6ca83ec9c73de855459a7a00
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2024-01-19 15:35:26 +0100

    Add a simple Webcomponent for <input>

---
+++ js/my-input.js
@@ -0,0 +1,107 @@
+// Many thanks to https://dev.to/stuffbreaker/custom-forms-with-web-components-and-elementinternals-4jaj
+customElements.define(
+  'my-input',
+  class extends HTMLElement {
+    static formAssociated = true;
+    static get observedAttributes() {
+      return ['required', 'value', 'placeholder'];
+    }
+
+		//  TS declarations
+    // $input: HTMLInputElement;
+    // _attrs = {};
+    // _internals: ElementInternals;
+    // _defaultValue = "";
+
+    constructor() {
+      super();
+			this._attrs = {};
+      this._internals = this.attachInternals();
+      this._internals.role = 'textbox';
+      this.tabindex = 0;
+    }
+
+    connectedCallback() {
+      this.attachShadow({
+        mode: 'open',
+        delegatesFocus: true,
+      }).innerHTML = `<input type="text" role="none" tabindex="-1" />`;
+      this.$input = this.shadowRoot.querySelector('input');
+      this.setProps();
+      this._defaultValue = this.$input.value;
+      this._internals.setFormValue(this.value);
+      this._internals.setValidity(
+        this.$input.validity,
+        this.$input.validationMessage,
+        this.$input
+      );
+      this.$input.addEventListener('input', () => this.handleInput());
+    }
+
+    attributeChangedCallback(name, _prev, next) {
+      this._attrs[name] = next;
+    }
+
+    formDisabledCallback(disabled) {
+      this.$input.disabled = disabled;
+    }
+
+    formResetCallback() {
+      this.$input.value = this._defaultValue;
+    }
+
+    checkValidity() {
+      return this._internals.checkValidity();
+    }
+
+    reportValidity() {
+      return this._internals.reportValidity();
+    }
+
+    get validity() {
+      return this._internals.validity;
+    }
+
+    get validationMessage() {
+      return this._internals.validationMessage;
+    }
+
+    setProps() {
+      // prevent any errors in case the input isn't set
+      if (!this.$input) {
+        return;
+      }
+
+      // loop over the properties and apply them to the input
+      for (let prop in this._attrs) {
+        switch (prop) {
+          case 'value':
+            this.$input.value = this._attrs[prop];
+            break;
+          case 'placeholder':
+            this.$input.placeholder = this._attrs[prop];
+            break;
+          case 'required':
+            const required = this._attrs[prop];
+            this.$input.toggleAttribute(
+              'required',
+              required === 'true' || required === ''
+            );
+            break;
+        }
+      }
+
+      // reset the attributes to prevent unwanted changes later
+      this._attrs = {};
+    }
+
+    handleInput() {
+      this._internals.setValidity(
+        this.$input.validity,
+        this.$input.validationMessage,
+        this.$input
+      );
+      this._internals.setFormValue(this.value);
+    }
+  }
+);
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,24 @@ async fn clicked() -> Markup {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
+    let template_routes = Router::new()
         .nest_service("/", ServeFile::new("templates/index.html"))
+        .nest_service("/get-form", ServeFile::new("templates/form.html"));
+
+    let dir_routes = Router::new()
         .nest_service("/assets", ServeDir::new("assets"))
-        .route("/clicked", post(clicked));
+        .nest_service("/js", ServeDir::new("js"));
+
+    let misc_routes =
+        Router::new().nest_service("/favicon.ico", ServeFile::new("assets/favicon.ico"));
+
+    let api_routes = Router::new().route("/clicked", post(clicked));
+
+    let app = Router::new()
+        .merge(template_routes)
+        .merge(dir_routes)
+        .merge(misc_routes)
+        .merge(api_routes);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
